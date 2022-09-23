b665c45c1e4123ef77d711131f4f2dab21ca9094
Duc Thai <ducthai060501@gmail.com>
2022-09-23 15:22:36 +0300

    business andfeatures

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const Button = () => {
+const Button:FC<any> = ({styles}) => {
   return (
-    <div>
-      
-    </div>
+    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    Get Started
+  </button>
   )
 }
 
